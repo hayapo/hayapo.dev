1cff7e91a81f45d8312d5a68490ac1472e95d6d0
hayapo <39045817+hayapo@users.noreply.github.com>
2024-06-30 07:10:23 +0900

    added blog link with icon

---
+++ src/lib/consts.ts
@@ -0,0 +1,17 @@
+export const TWITTER_ID = "_hayapo";
+export const GITHUB_ID = "hayapo";
+export const MASTODON_ID = "hayapo"
+export const DISCORD_ID = "hayaplayer";
+export const BLUESKY_ID = "hayapo.dev";
+export const ZENN_ID = "hayapo_2501";
+
+export const socials = {
+	twitter: TWITTER_ID,
+	github: GITHUB_ID,
+	mastodon: MASTODON_ID,
+	discord: DISCORD_ID,
+	bluesky: BLUESKY_ID,
+	zenn: ZENN_ID,
+}
+
+export const blogURL = "https://hayapo.dev/blog";
