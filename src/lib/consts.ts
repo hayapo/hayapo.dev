bd99f2a7dcf624169c927d9734ce406a77c930b4
hayapo <39045817+hayapo@users.noreply.github.com>
2024-06-30 07:49:57 +0900

    formatted by biome

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,6 +1,6 @@
 export const TWITTER_ID = "_hayapo";
 export const GITHUB_ID = "hayapo";
-export const MASTODON_ID = "hayapo"
+export const MASTODON_ID = "hayapo";
 export const DISCORD_ID = "hayaplayer";
 export const BLUESKY_ID = "hayapo.dev";
 export const ZENN_ID = "hayapo_2501";
@@ -12,6 +12,6 @@ export const socials = {
 	discord: DISCORD_ID,
 	bluesky: BLUESKY_ID,
 	zenn: ZENN_ID,
-}
+};
 
 export const blogURL = "https://hayapo.dev/blog";
